0ce0d3de60941bf752f7f62d3d647aa55f08b8c0
joungtcue <swcw99@naver.com>
2019-09-05 16:17:20 +0900

    1212

---
+++ WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Unit.java
@@ -0,0 +1,14 @@
+package kr.ac.daelim.uml;
+
+public class Unit {
+	public String name;
+	public int Health;
+	public void move()
+	{
+		System.out.println("유닛이 이동한다");
+	}
+	public void underattack(Charactor charactor)
+	{
+		System.out.println("유닛이 공격한다");
+	}
+}
