bb466392b05e60631212458e5302f4a5cbe69e31
joungtcue <swcw99@naver.com>
2019-09-05 16:35:04 +0900

    Unit

--- WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
+++ WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
@@ -1,5 +1,19 @@
 package kr.ac.daelim.uml;
 
-public class Marine {
+public class Marine extends Unit {
 
+	public void move()
+	{
+		System.out.println("유닛이 이동한다");
+	}
+	
+	public void attack(Charactor charactor)
+	{
+		System.out.println("유닛이 공격한다");
+	}
+	
+	public void underattack(Charactor charactor)
+	{
+		System.out.println("유닛이 공격받는다");
+	}
 }
